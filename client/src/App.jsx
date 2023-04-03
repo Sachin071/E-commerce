0b08290b6f98461ad83dd4814165eb231ecd744e
Shashankcognic <shashank.patel@cognicsys.com>
2023-04-03 12:57:36 +0530

    e-commerce template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/css/app.css'
+
+const App = () => {
+  return (
+    <>
+      <div className="app">
+        App
+      </div>
+    </>
+  )
+}
+
+export default App
